0df991fcbe1d63e8045e2701a6268628ed11f5e5
Mike Dunn <mdunn@safaribooksonline.com>
2018-07-30 17:07:24 -0500

    basic idea

---
+++ app/src/main/java/com/oreilly/oreillypager/CenterScalingPager.java
@@ -0,0 +1,122 @@
+package com.oreilly.oreillypager;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CenterScalingPager extends HorizontalScrollView {
+
+  private LinearLayout mLinearLayout;
+
+  private List<Integer> mCenters = new ArrayList<>();
+  private int mCenter;
+
+  public CenterScalingPager(@NonNull Context context) {
+    this(context, null);
+  }
+
+  public CenterScalingPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public CenterScalingPager(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    setFillViewport(true);
+    mLinearLayout = new LinearLayout(context);
+    LayoutParams lp = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    addView(mLinearLayout, lp);
+    for (int i = 0; i < 10; i++) {
+      TextView cell = new TextView(context);
+      cell.setPadding(20,20,20,20);
+      cell.setTextSize(72);
+      cell.setText(String.valueOf(i));
+      mLinearLayout.addView(cell);
+    }
+    mLinearLayout.addOnLayoutChangeListener(mOnLayoutChangeListener);
+  }
+
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    mCenter = getMeasuredWidth() / 2;
+  }
+
+  @Override
+  protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+    super.onScrollChanged(l, t, oldl, oldt);
+    if (mLastClosest != null) {
+      mLastClosest.setTextColor(Color.BLACK);
+    }
+    mLastClosest = findCenterMostChild();
+    if (mLastClosest != null) {
+      mLastClosest.setTextColor(Color.RED);
+    }
+  }
+
+  private TextView mLastClosest = null;
+  private TextView findCenterMostChild() {
+    int center = mCenter + getScrollX();
+    Log.d("CSP", "offset center: " + center);
+    for (int i = 0; i < mLinearLayout.getChildCount(); i++) {
+      View child = mLinearLayout.getChildAt(i);
+      Log.d("CSP", "i=" + i + ", left=" + child.getLeft() + ", right=" + child.getRight());
+      if (child.getLeft() < center && child.getRight() > center) {
+        // TODO: how far from center
+        float half = child.getMeasuredWidth() * 0.5f;
+        float middle = child.getLeft() + half;
+        float distance = Math.abs(middle - center);
+        float scale = 1 + (distance / half);
+        child.setScaleX(scale);
+        child.setScaleY(scale);
+        return (TextView) child;
+      }
+    }
+    return null;
+  }
+
+  private int getChildCenter(View view) {
+    return view.getLeft() + (view.getMeasuredWidth() / 2);
+  }
+
+  private void populateCenters() {
+    for (int i = 0; i < mLinearLayout.getChildCount(); i++) {
+      View child = mLinearLayout.getChildAt(i);
+      setCenterPosition(i, child.getLeft() + (child.getWidth() / 2));
+    }
+  }
+
+  private void setCenterPosition(int index, int position) {
+    if (mCenters.size() >= index) {
+      mCenters.add(position);
+    } else {
+      mCenters.set(index, position);
+    }
+  }
+
+  private View.OnLayoutChangeListener mOnLayoutChangeListener = new View.OnLayoutChangeListener() {
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+      if (mLinearLayout.getChildCount() <= 1) {
+        return;
+      }
+      View firstChild = mLinearLayout.getChildAt(0);
+      View lastChild = mLinearLayout.getChildAt(mLinearLayout.getChildCount() - 1);
+      int paddingLeft = mCenter - (firstChild.getWidth() / 2);
+      int paddingRight = mCenter - (lastChild.getWidth() / 2);
+      mLinearLayout.setPadding(paddingLeft, 0, paddingRight, 0);
+      populateCenters();
+    }
+  };
+
+}
--- app/src/main/java/com/oreilly/oreillypager/MainActivity.java
+++ app/src/main/java/com/oreilly/oreillypager/MainActivity.java
@@ -2,67 +2,13 @@ package com.oreilly.oreillypager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-  private HorizontalScrollView mScrollView;
-  private LinearLayout mLinearLayout;
-
-  private List<Integer> mCenters = new ArrayList<>(10);
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    mScrollView = findViewById(R.id.scrollview);
-    mLinearLayout = findViewById(R.id.linearlayout);
-    for (int i = 0; i < 10; i++) {
-      TextView cell = new TextView(this);
-      cell.setPadding(20,20,20,20);
-      cell.setTextSize(72);
-      cell.setText(String.valueOf(i));
-      mLinearLayout.addView(cell);
-    }
-    mLinearLayout.addOnLayoutChangeListener(mOnLayoutChangeListener);
   }
 
-  private void populateCenters() {
-    for (int i = 0; i < mLinearLayout.getChildCount(); i++) {
-      View child = mLinearLayout.getChildAt(i);
-      mCenters.set(i, child.getLeft() + (child.getWidth() / 2));
-    }
-  }
-
-  private View.OnScrollChangeListener mOnScrollChangeListener = new View.OnScrollChangeListener() {
-    @Override
-    public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-
-    }
-  };
-
-  private View.OnLayoutChangeListener mOnLayoutChangeListener = new View.OnLayoutChangeListener() {
-    @Override
-    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-      if (mLinearLayout.getChildCount() <= 1) {
-        return;
-      }
-      int totalWidth = mScrollView.getWidth();
-      int halfWidth = totalWidth / 2;
-      View firstChild = mLinearLayout.getChildAt(0);
-      View lastChild = mLinearLayout.getChildAt(mLinearLayout.getChildCount() - 1);
-      int firstChildWidth = firstChild.getWidth();
-      int lastChildWidth = lastChild.getWidth();
-      int paddingLeft = halfWidth - (firstChildWidth / 2);
-      int paddingRight = halfWidth - (lastChildWidth / 2);
-      mLinearLayout.setPadding(paddingLeft, 0, paddingRight, 0);
-      populateCenters();
-    }
-  };
 }
