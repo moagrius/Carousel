f13928f56132ac068cb3e00222c1b8d968f5c24b
Mike Dunn <mdunn@safaribooksonline.com>
2018-08-03 15:35:50 -0500

    working

--- app/src/main/java/com/oreilly/oreillypager/MainActivity.java
+++ app/src/main/java/com/oreilly/oreillypager/MainActivity.java
@@ -1,11 +1,18 @@
 package com.oreilly.oreillypager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
     mOverlay = findViewById(R.id.framelayout_overlay);
     mCard = findViewById(R.id.framelayout_card);
     mThumb = findViewById(R.id.imageview_card_thumb);
+    populateContinueReading();
+  }
+
+  private void populateContinueReading() {
+    try {
+      InputStream inputStream = getAssets().open("3.jpg");
+      Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+      ImageView lastThumb = findViewById(R.id.imageview_continue_reading_thumb);
+      lastThumb.setImageBitmap(bitmap);
+      TextView lastTextView = findViewById(R.id.textview_continue_reading_title);
+      String html = "<b>Continue:</b> Some title of some book";
+      lastTextView.setText(Html.fromHtml(html));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   private void onCellClick(View cell) {
